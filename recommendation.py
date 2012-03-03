99817e720ad91dd44ae6f87b7a92cc919cdf6b22
robbles <odwyerrob@gmail.com>
2012-03-03 15:00:03 -0800

    started working on the optimal next location algortihm

--- recommendation.py
+++ recommendation.py
@@ -1,9 +1,48 @@
 import sys
 import math
 
-def recommend_next_location(list_of_locations):
+def recommend_next_location(list_of_request_locations, list_of_closest_fountains):
+    
+    scale = 1000
+    max_points = find_max_and_min_points([list_of_request_locations])
+
+    potential_points = []
+
+    x_increment = (max_points[1] - max_points[0])/scale
+    y_increment = (max_points[3] - max_points[2])/scale
+
+    x_coords = []
+    y_coords = []
+
+    for i in range(scale):
+        x_coords.append(max_points[0]+i*x_increment)
+        y_coords.append(max_points[2]+i*y_increment)
+
+
+    for x in x_coords:
+        for y in y_coords:
+            potential_points.append((x,y))
+    
+    best_coordinate = ()
+    best_savings = 0
+          
+    for potential_point in potential_points:
+        
+        savings = 0
+        
+        for requested_location in list_of_requested_locations:
+            new_distance = find_distance(potential_point, requested_location)
+            closest_fountain_distance = find_distance(requested_location, closest_fountain) :
+            if new_distance < closest_fountain_distance:
+                savings = savings + (closest_fountain_distance-new_distance)
+
+        if savings > best_savings:
+            best_savings = savings
+            best_coordinate = potential point
    
-    return
+
+    return best_coordinate 
+)
 
 def find_max_and_min_points(list_of_locations):
     
