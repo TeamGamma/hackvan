c69f8173d1945f413d395b1c393ef574ada8b1fa
robbles <odwyerrob@gmail.com>
2012-03-23 16:20:57 -0700

    rename wsgi script for dotcloud

--- web/wsgi.py
+++ web/wsgi.py
@@ -2,7 +2,7 @@ import sys
 from os.path import dirname, abspath
 
 # Add the directory this WSGI script is in to the Python PATH
-sys.path.append(abspath(dirname(__file__)))
+#sys.path.append(abspath(dirname(__file__)))
 
 # Import the "app" object from __init__.py in this package and use it as the WSGI application
 from app import app as application
