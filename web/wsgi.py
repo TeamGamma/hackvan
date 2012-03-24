06cfefa0422d09b800d0a10a447b243648d1cf4b
robbles <odwyerrob@gmail.com>
2012-03-24 08:00:32 -0700

    fixed wsgi issue with dotcloud paths

--- web/wsgi.py
+++ web/wsgi.py
@@ -1,9 +1,8 @@
 import sys
-from os.path import dirname, abspath
 
-# Add the directory this WSGI script is in to the Python PATH
-#sys.path.append(abspath(dirname(__file__)))
+# Add the directory above this WSGI script to the Python PATH
+sys.path.append('/home/dotcloud/code')
 
 # Import the "app" object from __init__.py in this package and use it as the WSGI application
-from app import app as application
+from web import app as application
 
