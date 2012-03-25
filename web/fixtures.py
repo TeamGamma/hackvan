b1cc1c78fbb39237e6659d9859a9f51662ccf54a
alimjiwa <alimjiwa@gmail.com>
2012-03-24 17:10:58 -0700

    Update web/fixtures.py

--- web/fixtures.py
+++ web/fixtures.py
@@ -9,16 +9,21 @@ db.create_all()
 
 game = Game(id='game_zero', title='Game Zero', description='The first game',
     rounds=[
-        Round(round_number=0, answer='Lego', hints=[
-            Hint(position=2, hint="Initially only in red and white, but green blue and yellow came shortly after"),
-            Hint(position=0, hint="Invented in 1949 By Ole Christianson"),
-            Hint(position=1, hint="My company slogan is  'the best is never too good'"),
+        Round(round_number=0, answer='lego', hints=[
+            Hint(position=2, hint="I have my own theme park in Denmark. Over 200 billion of me have been produced"),
+            Hint(position=0, hint="I was Invented in 1949 By Ole Christianson"),
+            Hint(position=1, hint="I'm Made of molded plastic. Now come in 1700 shapes and every color"),
         ]),
-        Round(round_number=1, answer='Lego', hints=[
-            Hint(position=2, hint="Initially only in red and white, but green blue and yellow came shortly after"),
-            Hint(position=0, hint="Invented in 1949 By Ole Christianson"),
-            Hint(position=1, hint="My company slogan is  'the best is never too good'"),
+        Round(round_number=1, answer='everest', hints=[
+            Hint(position=2, hint="It is a landmark in Nepal"),
+            Hint(position=0, hint="It is known as Chomolungma in its native language"),
+            Hint(position=1, hint="It is the purpose of life for Edmund Hillary"),
         ]),
+        Round(round_number=2, answer='toes', hints=[
+            Hint(position=2, hint="They hide in your shoes and you have 10 of them"),
+            Hint(position=0, hint="They come in different sizes"),
+            Hint(position=1, hint="They are with you since you are born and help you walk"),
+        ]),        
     ],
 )
 
